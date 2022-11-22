d204db078dca916beafe94cd0d832db8e795a038
Илья <xxx_0589@mail.ru>
2022-11-22 11:18:59 +0300

    Home Work

---
+++ Seminar_1/Tack_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 1-ое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3-е число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine($"Наибольшее из введённых чисел {max}");
