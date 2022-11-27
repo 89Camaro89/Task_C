94b153b0a4adcc112691ed515b7c42263e1c502b
Илья <xxx_0589@mail.ru>
2022-11-27 17:50:07 +0300

    Home Work Seminar 2

---
+++ Seminar_2/Tack_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+int n = new Random().Next(100, 999);
+Console.WriteLine($"Случайное число {n}");
+string stringNumber = Convert.ToString(n);
+Console.WriteLine($"вторая цифра этого числа {stringNumber[1]}");
