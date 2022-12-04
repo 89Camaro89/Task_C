371839b3f936c8c0b560a8ad5deec4dff72efe3e
Илья <xxx_0589@mail.ru>
2022-12-04 15:44:52 +0300

    Seminar 3

---
+++ Seminar_3/Tack_21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координату X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y1 первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Z1 первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату X2 второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y2 второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Z2 второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)));
---
+++ Seminar_3/Tack_23/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Clear();
+int n = new Random().Next(0, 10);
+Console.WriteLine($"Слуайное число: {n} ");
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
