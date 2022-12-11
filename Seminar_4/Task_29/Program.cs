ba55531a39d69b3e824bf40d243238e608339b45
Илья <xxx_0589@mail.ru>
2022-12-11 11:39:55 +0300

    Seminar 4

---
+++ Seminar_4/Task_29/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int[] array = new int[8];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Веедите элемент массива: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    array[i] = x;
+}
+Console.Write($"Результат: [{string.Join(", ", array)}]");
+
