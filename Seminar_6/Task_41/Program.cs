ba9d7b405134e1894b55d24cfc0ca59fdeb7fd5a
Илья <xxx_0589@mail.ru>
2022-12-14 11:52:17 +0300

    Seminar 6

--- Seminar_6/Task_41/Program.cs
+++ Seminar_6/Task_41/Program.cs
@@ -1,38 +1,9 @@
-﻿Console.Clear();
-Console.WriteLine("Введите числа через запятую:");
-string numbers = Console.ReadLine();
-string[] newNumbers = new string[numbers.Length];
-int k = 0;
-for (int i = 0; i < newNumbers.Length; i++)
+﻿Console.Write("Введите кол-во чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
 {
-    if (numbers[i] == ',')
-    {
-        k++;
-    }
-    else
-    {
-        newNumbers[k] = newNumbers[k] + $"{numbers[i]}";
-    }
+    if (Convert.ToInt32(Console.ReadLine()) > 0)
+        count++;
 }
-k++;
-int[] resultNumbers = new int[k];
-PrintArray(resultNumbers, newNumbers);
-int sum = 0;
-for (int i = 0; i < k; i++)
-{
-    if (resultNumbers[i] > 0)
-    {
-        sum++;
-    }
-}
-Console.WriteLine($"Количество чисел больше 0: {sum} ");
-
-void PrintArray(int[] array, string[] string1)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = Convert.ToInt32(string1[i]);
-        Console.Write(array[i] + "  ");
-    }
-    Console.WriteLine();
-}
+Console.WriteLine(count);
